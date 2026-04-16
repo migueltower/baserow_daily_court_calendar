7885e90520dd0e0beed7cc7639dd4745b94dc90a
migueltower <106282745+migueltower@users.noreply.github.com>
2026-04-16 10:53:51 -0700

    Update filter to exclude floor 4 in fetch.py

--- fetch.py
+++ fetch.py
@@ -100,8 +100,8 @@ def scrape_data() -> List[Dict[str, Any]]:
         time_str = cols[4].get_text(strip=True)
         case_number = cols[5].get_text(strip=True)
 
-        # Filter out floors 2 and 3, and times before 9:00 AM
-        if floor in ["2", "3"]:
+        # Filter out floors 2 and 3 and 4, and times before 9:00 AM
+        if floor in ["2", "3", "4"]:
             continue
         if not time_is_after_nine_am(time_str):
             continue
